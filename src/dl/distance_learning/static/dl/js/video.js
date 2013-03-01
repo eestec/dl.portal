1729b16e4c2ff84c6343154e9d3340be43879e7c
Marko Lalic <marko.lalic@gmail.com>
2013-03-01 19:21:54 +0100

    Refined the linkification of the video description.

--- src/dl/distance_learning/static/dl/js/video.js
+++ src/dl/distance_learning/static/dl/js/video.js
@@ -40,4 +40,5 @@ $(function() {
         });
     });
 
+    $('#video-description').linkify();
 });
--- src/dl/distance_learning/templatetags/distance_learning_tags.py
+++ src/dl/distance_learning/templatetags/distance_learning_tags.py
@@ -34,7 +34,6 @@ def link_urls(value):
     <a> tags.
     When used in a template, the resulting value needs to be marked as safe.
     """
-    regexp = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
     regexp = (
         """http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|"""
         """(?:%[0-9a-fA-F][0-9a-fA-F]))+"""
